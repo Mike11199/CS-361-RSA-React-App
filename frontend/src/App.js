602749f18e78dfe0034be7724413a8aebc4522a4
Michael Iwanek <miwanek11199@gmail.com>
2023-04-12 00:00:33 -0700

    separated rsa page into component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
 import { Routes, Route, Navigate } from 'react-router-dom'
+import RSAPage from './components/RSAPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<h1>Home</h1>}></Route>
-      <Route path="/rsa" element={<h1>RSA Page</h1>}></Route>
+      <Route path="/rsa" element={<RSAPage />}></Route>
       <Route path="*" element={<Navigate to="/" />}></Route>  
     </Routes>
   );
---
+++ frontend/src/components/RSAPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RSAPage = () => {
+  return (
+    <div>
+      <h1>RSA Page</h1>
+      <p>This is the RSA page.</p>
+    </div>
+  );
+}
+
+export default RSAPage;
