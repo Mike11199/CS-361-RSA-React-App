27b68d5165dea886cb828a103c1d2877d1863b43
Michael Iwanek <miwanek11199@gmail.com>
2023-04-14 23:45:19 -0700

    addd styled component to make textarea bigger on rsa page.  confirmed button does generate rsa key public and private.

--- frontend/src/components/RSAPage.js
+++ frontend/src/components/RSAPage.js
@@ -2,29 +2,40 @@ import React from 'react';
 import { JSEncrypt } from "jsencrypt";  // reference https://bartlomiejmika.com/post/2022/how-to-perform-rsa-encryption-in-javascript-and-golang/
 //https://www.npmjs.com/package/node-rsa   THIS DID NOT WORK, ONLY FOR NODE AND NOT FOR REACT
 
+import Wrapper from '../wrappers/RSA.js'  // this is for styled components
+
 const generateRSAKeys = () => {
   // reference https://bartlomiejmika.com/post/2022/how-to-perform-rsa-encryption-in-javascript-and-golang/
   let crypt = new JSEncrypt({default_key_size: 2048}) 
   let publicKey = crypt.getPublicKey()
   let privateKey = crypt.getPrivateKey()
+  // let publicKey_B64 = crypt.getPublicKeyB64()
+  // let privateKey_B64 = crypt.getPrivateKeyB64()
   console.log(crypt)
   console.log(publicKey)
   console.log(privateKey)
+  const private_key_field = document.getElementById("rsa_private_key_field")
+  const public_key_field = document.getElementById("rsa_public_key_field")
+  private_key_field.innerText = privateKey
+  public_key_field.innerText = publicKey
+  // console.log(publicKey_B64)
+  // console.log(privateKey_B64)
 }
 
 
 
 const RSAPage = () => {
   return (
+    <Wrapper>
     <div>
       <h1>RSA Page</h1>
       <p>This is the RSA page.</p>
       <button onClick={()=> generateRSAKeys()}>Generate RSA Key Pair</button>
       <br></br>
       <p>RSA Private Key</p>
-      <textarea></textarea>
+      <textarea id="rsa_private_key_field"></textarea>
       <p>RSA Public Key</p>
-      <textarea></textarea>
+      <textarea id="rsa_public_key_field"></textarea>
       <br></br>
       <br></br>
       <button>Encrypt with RSA Public Key</button>
@@ -43,7 +54,11 @@ const RSAPage = () => {
       <textarea></textarea>
       <p>Text Decrypted with Private Key</p>
       <textarea></textarea>
+      <br></br>
+      <br></br>
+      <br></br>
     </div>
+    </Wrapper>
   );
 }
 
---
+++ frontend/src/wrappers/RSA.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+const Wrapper = styled.aside`
+    textarea {  
+      width: 65%;
+      height: 20vh;
+    }
+`
+export default Wrapper
