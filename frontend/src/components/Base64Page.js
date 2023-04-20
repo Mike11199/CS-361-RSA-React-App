80d48f999fb0fd3d2153cb9c6f95b3cc5dd1c65d
Michael Iwanek <miwanek11199@gmail.com>
2023-04-19 20:07:54 -0700

    added image to test encrypting an image

--- frontend/src/components/Base64Page.js
+++ frontend/src/components/Base64Page.js
@@ -1,10 +1,13 @@
 
+import image from "../images/link_image.png"
 
 const Base64Page = () => {
   return (
     <>
     <div className="pageContainer" style={{paddingLeft:"30px"}}>
-    <p>You can use the below to encrypt/decrypt a file!</p>
+    <p>You can use the below external links to encrypt/decrypt a file!</p>
+    <p>Try to encode/decode this image!  This image is small so that this can work.</p>
+    <img src={image} alt="link_image" style={{height: "100px", marginBottom: '20px'}} />
     <div>
       <li><a href="https://base64.guru/converter/decode/file">Base 64 to File Converter (External Site)</a></li>
       <li><a href="https://base64.guru/converter/encode/file">File to Base 64 Converter (External Site)</a></li>
