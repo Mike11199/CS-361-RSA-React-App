8b8a9c4fb187d52db83d98fae42fa80a56525be9
Michael Iwanek <miwanek11199@gmail.com>
2023-04-20 11:27:51 -0700

    updated RSA page to use state variables instead of manually getting textarea values

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,3 +1,4 @@
+import image from "../images/key_crypto_image.png"
 
 const HomePage = () => {
   return (
@@ -5,6 +6,7 @@ const HomePage = () => {
         <div className="pageContainer" style={{paddingLeft:"30px"}}>
         <h1>Home Page</h1>
         <p>Welcome to my app!  Please click the links to navigate.</p>
+        <img src={image} alt="link_image" style={{height: "400px", marginBottom: '20px'}} />
         </div>
         </>
   );
--- frontend/src/components/RSAPage.js
+++ frontend/src/components/RSAPage.js
@@ -1,54 +1,70 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { JSEncrypt } from "jsencrypt";  // reference https://bartlomiejmika.com/post/2022/how-to-perform-rsa-encryption-in-javascript-and-golang/
 //https://www.npmjs.com/package/node-rsa   THIS DID NOT WORK, ONLY FOR NODE AND NOT FOR REACT
-
 import Wrapper from '../wrappers/RSA.js'  // this is for styled components
 
-const generateRSAKeys = () => {
-  // reference https://bartlomiejmika.com/post/2022/how-to-perform-rsa-encryption-in-javascript-and-golang/
-  let crypt = new JSEncrypt({default_key_size: 2048}) 
-  let publicKey = crypt.getPublicKey()
-  let privateKey = crypt.getPrivateKey()
-  // let publicKey_B64 = crypt.getPublicKeyB64()
-  // let privateKey_B64 = crypt.getPrivateKeyB64()
-  console.log(crypt)
-  console.log(publicKey)
-  console.log(privateKey)
-  const private_key_field = document.getElementById("rsa_private_key_field")
-  const public_key_field = document.getElementById("rsa_public_key_field")
-  private_key_field.value = privateKey
-  public_key_field.value = publicKey
-  // console.log(publicKey_B64)
-  // console.log(privateKey_B64)
-}
 
-const encryptWithRSAPublicKey = () => {
-  let public_key_field = document.getElementById("rsa_public_key_field")
-  let rsa_public_key_from_textarea = public_key_field.value  //has to be innerHTML not innerText for some reason
-  // console.log(rsa_public_key_from_textarea)
+const RSAPage = () => {
 
-    //check if RSA public key is empty
-    if (!rsa_public_key_from_textarea) {
-      console.log("RSA public key is empty");
-      window.alert("RSA public key is empty!");
-      return;
-    }
+  const [privateKey, setPrivateKey] = useState('');
+  const [publicKey, setPublicKey] = useState('');
+
+  const generateRSAKeys = () => {
   
-  let text_to_encrypt_with_pub_key_field = document.getElementById("text_to_encrypt_with_pub_key")
-  let text_to_encrypt_with_pub_key= text_to_encrypt_with_pub_key_field.value
-
-      //check if text to encrypt with RSA public key is empty
-      if (!text_to_encrypt_with_pub_key) {
-        console.log("Text to encrypt with RSA public key is empty");
-        window.alert("Text to encrypt with RSA public key is empty!");
+    //check to make sure user does not overwrite old keys on accident
+    // https://developer.mozilla.org/en-US/docs/Web/API/Window/confirm
+    // ok = true/ cancel = false
+    if (privateKey || publicKey) {
+      if (window.confirm("Are you sure you want to overwrite existing fields?")) {
+        window.alert("Generating your keys!");
+        setPrivateKey("");
+        setPublicKey("");
+      }
+      else{
+        window.alert("Canceled key pair generation!");
         return;
       }
+    }
+
+    // reference https://bartlomiejmika.com/post/2022/how-to-perform-rsa-encryption-in-javascript-and-golang/
+    let crypt = new JSEncrypt({default_key_size: 2048})    
+    let publicKey_generated = crypt.getPublicKey()  //can't pass directly to set state or error
+    let privateKey_generated  = crypt.getPrivateKey()
+
+    setPrivateKey(privateKey_generated)
+    setPublicKey(publicKey_generated)
+
+    console.log('generated from crypt')
+    console.log(crypt)
+    console.log(privateKey_generated)
+    console.log(publicKey_generated)
+  }
+
+
+  const encryptWithRSAPublicKey = () => {
+
+      //check if RSA public key is empty
+      if (!publicKey) {
+        console.log("RSA public key is empty");
+        window.alert("RSA public key is empty!");
+        return;
+      }
+    
+    let text_to_encrypt_with_pub_key_field = document.getElementById("text_to_encrypt_with_pub_key")
+    let text_to_encrypt_with_pub_key= text_to_encrypt_with_pub_key_field.value
+
+        //check if text to encrypt with RSA public key is empty
+        if (!text_to_encrypt_with_pub_key) {
+          console.log("Text to encrypt with RSA public key is empty");
+          window.alert("Text to encrypt with RSA public key is empty!");
+          return;
+        }
     
 
   let crypt2 = new JSEncrypt() 
 
   // reference https://www.npmjs.com/package/jsencrypt
-  crypt2.setPublicKey(rsa_public_key_from_textarea)
+  crypt2.setPublicKey(publicKey)
 
   // window.alert(text_to_encrypt_with_pub_key);
   let encrypted_string_with_public_key = crypt2.encrypt(text_to_encrypt_with_pub_key)
@@ -59,57 +75,55 @@ const encryptWithRSAPublicKey = () => {
   let encrypted_text_with_pub_key_field = document.getElementById("text_encrypted_with_pub_key")
   encrypted_text_with_pub_key_field.value = encrypted_string_with_public_key
 
-}
-
-
-const decryptWithPrivateKey = () => {
-
-  // <textarea id="rsa_private_key_field" style={{height:'450px'}}></textarea>
-  // <textarea id="text_to_decrypt_with_private_key"></textarea>
-  // <p>Text Decrypted with Private Key</p>
-  // <textarea id="decrypted_text_using_private_key"></textarea>
-
-  let private_key_field = document.getElementById("rsa_private_key_field")
-  let rsa_private_key_from_textarea = private_key_field.value  //has to be innerHTML not innerText for some reason
-  // console.log(rsa_private_key_from_textarea)
-
-  //check if RSA private key is empty
-  if (!rsa_private_key_from_textarea) {
-    console.log("RSA private key is empty");
-    window.alert("RSA private key is empty!");
-    return;
   }
 
-  
-  let text_to_decrypt_with_private_key_field = document.getElementById("text_to_decrypt_with_private_key")
-  let text_to_decrypt_with_private_key= text_to_decrypt_with_private_key_field.value
 
-      //check if text to encrypt with RSA public key is empty
-      if (!text_to_decrypt_with_private_key) {
-        console.log("Text to decrypt with RSA private key is empty");
-        window.alert("Text to decrypt with RSA private key is empty!");
-        return;
-      }
+  const decryptWithPrivateKey = () => {
+
+    // <textarea id="rsa_private_key_field" style={{height:'450px'}}></textarea>
+    // <textarea id="text_to_decrypt_with_private_key"></textarea>
+    // <p>Text Decrypted with Private Key</p>
+    // <textarea id="decrypted_text_using_private_key"></textarea>
+
+    let private_key_field = document.getElementById("rsa_private_key_field")
+    let rsa_private_key_from_textarea = private_key_field.value  //has to be innerHTML not innerText for some reason
+    // console.log(rsa_private_key_from_textarea)
+
+    //check if RSA private key is empty
+    if (!rsa_private_key_from_textarea) {
+      console.log("RSA private key is empty");
+      window.alert("RSA private key is empty!");
+      return;
+    }
+
     
+    let text_to_decrypt_with_private_key_field = document.getElementById("text_to_decrypt_with_private_key")
+    let text_to_decrypt_with_private_key= text_to_decrypt_with_private_key_field.value
+
+        //check if text to encrypt with RSA public key is empty
+        if (!text_to_decrypt_with_private_key) {
+          console.log("Text to decrypt with RSA private key is empty");
+          window.alert("Text to decrypt with RSA private key is empty!");
+          return;
+        }
+      
 
 
-  let crypt3 = new JSEncrypt()
+    let crypt3 = new JSEncrypt()
 
-  
-  // reference https://www.npmjs.com/package/jsencrypt
-  crypt3.setPrivateKey(rsa_private_key_from_textarea)
-  let decrypted_string_with_private_key = crypt3.decrypt(text_to_decrypt_with_private_key)
+    
+    // reference https://www.npmjs.com/package/jsencrypt
+    crypt3.setPrivateKey(rsa_private_key_from_textarea)
+    let decrypted_string_with_private_key = crypt3.decrypt(text_to_decrypt_with_private_key)
 
-  
-  // set field to decrypted text
-  let decrypted_text_with_private_key_field = document.getElementById("decrypted_text_using_private_key")
-  decrypted_text_with_private_key_field.value = decrypted_string_with_private_key
-
-
-}
+    
+    // set field to decrypted text
+    let decrypted_text_with_private_key_field = document.getElementById("decrypted_text_using_private_key")
+    decrypted_text_with_private_key_field.value = decrypted_string_with_private_key
 
 
-const RSAPage = () => {
+  }
+
   return (
     <Wrapper>
     <div className="pageContainer">
@@ -118,9 +132,9 @@ const RSAPage = () => {
       <button className="button_purple" onClick={()=> generateRSAKeys()}>Generate RSA Key Pair</button>
       <br></br>
       <p>RSA Private Key</p>
-      <textarea id="rsa_private_key_field" style={{height:'450px'}}></textarea>
+      <textarea value={privateKey} id="rsa_private_key_field" style={{height:'450px'}} onChange={(e) => setPrivateKey(e.target.value)} ></textarea>
       <p>RSA Public Key</p>
-      <textarea id="rsa_public_key_field" style={{height:'150px'}}></textarea>
+      <textarea value={publicKey} id="rsa_public_key_field" style={{height:'150px'}} onChange={(e) => setPublicKey(e.target.value)} ></textarea>
       <br></br>
       <br></br>
       <button className="button_green" onClick={()=> encryptWithRSAPublicKey()}>Encrypt Text with RSA Public Key</button>
