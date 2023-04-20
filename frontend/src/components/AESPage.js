305289c4621ff549d815442acda9bcc716a4ef54
Michael Iwanek <miwanek11199@gmail.com>
2023-04-19 18:45:20 -0700

    got AES key generation, encryption, and decryption working

--- frontend/src/components/AESPage.js
+++ frontend/src/components/AESPage.js
@@ -1,53 +1,142 @@
 import React from 'react';
-import { JSEncrypt } from "jsencrypt";  // reference https://bartlomiejmika.com/post/2022/how-to-perform-rsa-encryption-in-javascript-and-golang/
+// import { JSEncrypt } from "jsencrypt";  // reference https://bartlomiejmika.com/post/2022/how-to-perform-rsa-encryption-in-javascript-and-golang/
 //https://www.npmjs.com/package/node-rsa   THIS DID NOT WORK, ONLY FOR NODE AND NOT FOR REACT
+import CryptoJS from 'crypto-js';
+
+// https://github.com/brix/crypto-js
+// https://www.npmjs.com/package/crypto-js
+
 
 import Wrapper from '../wrappers/AES.js'  // this is for styled components
 
 const generateAESKey = () => {
 
+    // reference https://cryptojs.gitbook.io/docs/ for how to encrypt and decrypt with AES 
+    // Generate a random 256-bit key
+   
+    // reference https://cryptojs.gitbook.io/docs/#the-hashing-input
+    // The hash algorithms accept either strings or instances of CryptoJS.lib.WordArray. 
+    // A WordArray object represents an array of 32-bit words. 
+    // When you pass a string, it's automatically converted to a WordArray encoded as UTF-8.
+
+    const AES_Key = CryptoJS.lib.WordArray.random(32)  //32 bytes * 8 = 256 bit key!
+
+    // can't use utf here or crashes.. 
+    // I believe it converts the bits directly to hex so that's why
+    const AES_Key_String = AES_Key.toString(CryptoJS.enc.Hex) // this converts the WordArray object to a hex string
+   
+    console.log(AES_Key)
+    console.log(AES_Key_String)
+    let AES_generated_key_field = document.getElementById("aes_key_field")
+    AES_generated_key_field.value = AES_Key_String
+
 }
 
 const encryptWithAESKey = () => {
+
+  let AES_generated_key_field = document.getElementById("aes_key_field")
+  const AES_Key_String = AES_generated_key_field.value 
+
   
+    //check if RSA public key is empty
+    if (!AES_Key_String) {
+      console.log("AES key field is empty");
+      window.alert("AES key field is empty!");
+      return;
+    }
+
+  console.log(AES_Key_String)
+
+
+  // const message = "test"
+
+  let AES_text_to_encrypt_field = document.getElementById("text_to_encrypt_with_aes_key")
+  const AES_text_to_encrypt = AES_text_to_encrypt_field.value 
+
+  
+    //check if RSA public key is empty
+    if (!AES_text_to_encrypt) {
+      console.log("AES text to encrypt field is empty");
+      window.alert("AES text to encrypt field is empty!");
+      return;
+    }
+
+
+
+  // Encrypt the message with the key
+  const encrypted = CryptoJS.AES.encrypt(AES_text_to_encrypt, AES_Key_String);
+  console.log(encrypted)
+  console.log(encrypted.toString())
+
+  let AES_encrypted_text_field = document.getElementById("text_encrypted_with_aes_key")
+  AES_encrypted_text_field.value = encrypted.toString()
+
 }
 
 
 const decryptWithAESKey = () => {
 
- 
+  let AES_generated_key_field = document.getElementById("aes_key_field")
+  let AES_Key_String2 = AES_generated_key_field.value 
+
+  
+    //check if RSA public key is empty
+    if (!AES_Key_String2) {
+      console.log("AES key field is empty");
+      window.alert("AES key field is empty!");
+      return;
+    }
+
+
+  let AES_text_to_decrypt_field = document.getElementById("text_to_decrypt_with_aes_key")
+  let AES_text_to_decrypt = AES_text_to_decrypt_field.value
+
+    //check if AES text to decrypt field is empty and show alert if is
+    if (!AES_text_to_decrypt) {
+      console.log("AES text to decrypt field is empty");
+      window.alert("AES text to decrypt field is empty!");
+      return;
+    }
+
+  const decrypted = CryptoJS.AES.decrypt(AES_text_to_decrypt, AES_Key_String2);
+  console.log(decrypted.toString(CryptoJS.enc.Utf8));
+
+  let AES_decrypted_text_field = document.getElementById("decrypted_text_using_aes_key")
+  AES_decrypted_text_field.value = decrypted.toString(CryptoJS.enc.Utf8)
+
 }
+
 
 
 const AESPage = () => {
   return (
     <Wrapper>
-    <div class="pageContainer">
+    <div className="pageContainer">
       <h1>AES Page</h1>
       <p>This is the AES page.</p>
-      <button class="button_purple" onClick={()=> generateAESKey()}>Generate New AES Key</button>
+      <button className="button_purple" onClick={()=> generateAESKey()}>Generate New AES Key</button>
       <br></br>
       <p>AES Symmetric Key</p>
-      <textarea id="rsa_private_key_field" style={{height:'150px'}}></textarea>
+      <textarea id="aes_key_field" style={{height:'150px'}}></textarea>
       
       <br></br>
       <br></br>
-      <button class="button_green" onClick={()=> encryptWithAESKey()}>Encrypt Text with AES Key</button>
+      <button className="button_green" onClick={()=> encryptWithAESKey()}>Encrypt Text with AES Key</button>
       <br></br>
       <br></br>
       <p>Text to Encrypt</p>      
-      <textarea id="text_to_encrypt_with_pub_key"></textarea>
+      <textarea id="text_to_encrypt_with_aes_key"></textarea>
       <p>Text Encrypted with AES Key</p>
-      <textarea id="text_encrypted_with_pub_key"></textarea>
+      <textarea id="text_encrypted_with_aes_key"></textarea>
       <br></br>
       <br></br>
-      <button class="button_red" onClick={()=> decryptWithAESKey()}>Decrypt Text with AES Key</button>
+      <button className="button_red" onClick={()=> decryptWithAESKey()}>Decrypt Text with AES Key</button>
       <br></br>
       <br></br>
       <p>Text to Decrypt</p>      
-      <textarea id="text_to_decrypt_with_private_key"></textarea>
+      <textarea id="text_to_decrypt_with_aes_key"></textarea>
       <p>Text Decrypted with AES Key</p>
-      <textarea id="decrypted_text_using_private_key"></textarea>
+      <textarea id="decrypted_text_using_aes_key"></textarea>
       <br></br>
       <br></br>
       <br></br>
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
         <>
-        <div class="navbar" >
+        <div className="navbar">
             <Link to="/">Home</Link>
             <Link to="/rsa">RSA</Link>
             <Link to="/aes">AES</Link>
