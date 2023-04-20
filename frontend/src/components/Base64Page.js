f3733ca41ad9acb6e09b3acbdb8a01cf02685913
Michael Iwanek <miwanek11199@gmail.com>
2023-04-19 19:57:18 -0700

    change inline style for padding for home and b64 page

--- frontend/src/components/Base64Page.js
+++ frontend/src/components/Base64Page.js
@@ -3,7 +3,7 @@
 const Base64Page = () => {
   return (
     <>
-    <div className="pageContainer" style={{paddingLeft:"20px"}}>
+    <div className="pageContainer" style={{paddingLeft:"30px"}}>
     <p>You can use the below to encrypt/decrypt a file!</p>
     <div>
       <li><a href="https://base64.guru/converter/decode/file">Base 64 to File Converter (External Site)</a></li>
--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -2,8 +2,10 @@
 const HomePage = () => {
   return (
         <>
+        <div className="pageContainer" style={{paddingLeft:"30px"}}>
         <h1>Home Page</h1>
         <p>Welcome to my app!  Please click the links to navigate.</p>
+        </div>
         </>
   );
 }
